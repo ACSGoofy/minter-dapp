9706df6033e3c66a23700337a37fe8b0a698b077
ACGGoofy <austinwagers@gmail.com>
2022-05-13 17:38:06 -0400

    k

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,13 +65,13 @@ const PUBLIC_MINT_START_DATE = "2022-06-01T12:00:00+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-05-13T12:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0xE1AA4f59a50bEe336E1005D9318daF1469cA7939"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = ["0xB89f9F7acdB867fd591b310594F395BB7Fe2E395"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x7DBd1F5364Ba2834a3B3255597e7A080227763C8"; // If you want to manually include it
+let CONTRACT_ADDRESS = "00xE1AA4f59a50bEe336E1005D9318daF1469cA7939"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
