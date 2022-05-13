696f737a6d87d63641e29ec849c88bb57d2d4e0b
ACGGoofy <austinwagers@gmail.com>
2022-05-13 17:03:32 -0400

    h

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0xe01a529FbF5EFB1708C2D223E2EE33d3fC977514"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x7DBd1F5364Ba2834a3B3255597e7A080227763C8"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0xe01a529FbF5EFB1708C2D223E2EE33d3fC977514"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x7DBd1F5364Ba2834a3B3255597e7A080227763C8"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Acid Cat NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
